dbd470873dd9bd39f2e8da85189374a69507c300
KiranKumar4024 <139195365+KiranKumar4024@users.noreply.github.com>
2023-07-14 12:01:43 +0530

    Update postfixEval.c

--- Day-5(DAA)/postfixEval.c
+++ Day-5(DAA)/postfixEval.c
@@ -3,31 +3,48 @@
 int push(int top,int n,int* stack){
     top=top+1;
     stack[top]=n;
-    printf("yes");
-    printf("%d",top);
+   // printf("yes");
+   // printf("%d",top);
     return top;
 }
 int  pop(int top)
 {
     top=top-1;
 }
-void op(char a,int* stack,int top){
+int op(char a,int* stack,int top){
+    int res;
     switch(a){
-        int res;
+
         case '+':
             res=stack[top]+stack[top-1];
-            top=pop(top);
-            top=pop(top-1);
-            printf("%d",top);
-            push(top,res,stack);
             break;
+        case '-':
+            res=stack[top]-stack[top-1];
+            break;
+        case '*':
+            res=stack[top]*stack[top-1];
+            break;
+        case '/':
+            res=stack[top]/stack[top-1];
+            break;
+        default:
+            printf("INVALID");
+            break;
+
     //printf("add");
     }
+      top=pop(top);
+            top=pop(top-1);
+            push(top,res,stack);
+            //printf("%d",top);
+            //printf("%d",res);
+        return res;
 }
 
 void main(){
     int stck[50];
     int top=-1;
+    int res;
 char a[50];
 char b='#';
 printf("Enter a string:");
@@ -42,12 +59,15 @@ for (int i=0;i<strlen(a);i++){
         top=push(top,(int)a[i]-48,stck);
     }
     else if (a[i]=='+' || a[i]=='-' || a[i]=='*' || a[i]=='/'){
-        op(a[i],stck,top);
+        res=op(a[i],stck,top);
     }
     else if (a[i]=='#'){
+        goto lb;
         break;
     }
 
 }
 printf("outside");
+lb:
+    printf("RESULT:%d",res);
 }
