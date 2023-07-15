a35a46e4d62d9ed9d9812a50f4067fceac509014
KiranKumar4024 <139195365+KiranKumar4024@users.noreply.github.com>
2023-07-15 18:29:38 +0530

    Update postfixEval.c

--- Day-5(DAA)/postfixEval.c
+++ Day-5(DAA)/postfixEval.c
@@ -1,10 +1,9 @@
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
 int push(int top,int n,int* stack){
     top=top+1;
     stack[top]=n;
-   //printf("yes");
-printf("%d\n",top);
     return top;
 }
 int  pop(int top)
@@ -12,71 +11,57 @@ int  pop(int top)
     top=top-1;
     return top;
 }
-void op(char a,int* stack,int top){
+int op(char a,int* stack,int top){
     int res;
     switch(a){
-
         case '+':
             res=stack[top]+stack[top-1];
-            printf("%d\t%d\n",stack[top],stack[top-1]);
-            printf("add\n");
             break;
         case '-':
             res=stack[top-1]-stack[top];
-            printf("%d\t%d\n",stack[top],stack[top-1]);
-            printf("sub\n");
             break;
         case '*':
             res=stack[top]*stack[top-1];
-            printf("%d\t%d\n",stack[top],stack[top-1]);
-            printf("mul\n");
             break;
         case '/':
             res=stack[top-1]/stack[top];
-            printf("%d\t%d\n",stack[top],stack[top-1]);
-            printf("div\n");
+            break;
+        case '^':
+            res=stack[top-1]^stack[top];
+            break;
+        case '%':
+            res=stack[top-1]%stack[top];
             break;
         default:
-            printf("INVALID");
-            break;
-
-    //printf("add");
+            exit(1);
     }
             top=pop(top);
-            top=pop(top-1);
-            push(top,res,stack);
-            printf("%d\n",top);
-            printf("%d\n",res);
-        //return res;
+            top=pop(top);
+            top=push(top,res,stack);
+    return top;
 }
-
 void main(){
     int stck[50];
     int top=-1;
     int res;
-char a[50];
-char b='#';
-printf("Enter a string:");
-scanf("%s",&a);
-strncat(a,&b,1);
-/*printf("%s",a);
-printf("%d",strlen(a));*/
-//printf("%d",5(char)(int)'+'9);
-for (int i=0;i<strlen(a);i++){
-    if(((int)a[i]-48)>0 && ((int)a[i]-48)<=9)
+    char a[50];
+    char b='#';
+    printf("Enter a string:");
+    scanf("%s",&a);
+    strncat(a,&b,1);
+    for (int i=0;i<strlen(a);i++){
+        if(((int)a[i]-48)>0 && ((int)a[i]-48)<=9)
         {
-        top=push(top,(int)a[i]-48,stck);
-    }
-    else if (a[i]=='+' || a[i]=='-' || a[i]=='*' || a[i]=='/'){
-        op(a[i],stck,top);
-    }
-    else if (a[i]=='#'){
-        //goto lb;
+            top=push(top,(int)a[i]-48,stck);
+        }
+        else if (a[i]=='+' || a[i]=='-' || a[i]=='*' || a[i]=='/' || a[i]=='^' || a[i]=='%' )
+            {
+            top=op(a[i],stck,top);
+            }
+        else if (a[i]=='#')
+        {
+        printf("\n\tRESULT:%d",stck[top]);
         break;
-    }
-
+        }
 }
-printf("outside");
-/*lb:
-    printf("RESULT:%d",res);*/
 }
